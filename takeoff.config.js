ecba0b6d3dfde9aabdc95da519326c16898f4a7d
Tane Piper <piper.tane@gmail.com>
2017-10-26 01:12:04 +0200

    Add progress bar

--- takeoff.config.js
+++ takeoff.config.js
@@ -1,9 +1,16 @@
 let sleep = 'sleep 5';
 if (process.platform === 'win32') sleep = 'sleep -s 5';
 
-module.exports = ({ command, shell, args, opts, workingDir }) => {
+module.exports = ({ command, shell, args, opts, workingDir, ProgressBar }) => {
+
+  const bar = new ProgressBar({
+    total: 4
+  });
+
     const submoduleInit = shell.exec(`npm install`, { cwd: __dirname, silent: opts.v ? false : true });
     if (submoduleInit.code !== 0) return false;
+
+    bar.tick();
 
     const bootstrap = shell.exec(`node_modules/.bin/lerna bootstrap`, {
         cwd: __dirname,
@@ -11,11 +18,15 @@ module.exports = ({ command, shell, args, opts, workingDir }) => {
     });
     if (bootstrap.code !== 0) return false;
 
+    bar.tick();
+
     const build = shell.exec(`docker-compose -f docker/docker-compose.yml build --no-cache`, {
         cwd: __dirname,
         silent: opts.v ? false : true
     });
     if (build.code !== 0) return false;
+
+    bar.tick();
 
     const dbinit = shell.exec(
         `docker-compose -f docker/docker-compose.yml build --no-cache \
@@ -23,6 +34,8 @@ module.exports = ({ command, shell, args, opts, workingDir }) => {
         { cwd: __dirname, silent: opts.v ? false : true }
     );
     if (dbinit.code !== 0) return false;
+
+    bar.tick();
 
     return true;
 };
