34ce5ce740d93fab8df35a01470a0b5e8599823b
Tane Piper <piper.tane@gmail.com>
2017-10-26 01:03:29 +0200

    Use opts

--- takeoff.config.js
+++ takeoff.config.js
@@ -1,26 +1,26 @@
 let sleep = 'sleep 5';
 if (process.platform === 'win32') sleep = 'sleep -s 5';
 
-module.exports = ({ command, shell, args, options, workingDir }) => {
-    const submoduleInit = shell.exec(`npm install`, { cwd: __dirname, silent: options.v ? false : true });
+module.exports = ({ command, shell, args, opts, workingDir }) => {
+    const submoduleInit = shell.exec(`npm install`, { cwd: __dirname, silent: opts.v ? false : true });
     if (submoduleInit.code !== 0) return false;
 
     const bootstrap = shell.exec(`node_modules/.bin/lerna bootstrap`, {
         cwd: __dirname,
-        silent: options.v ? false : true
+        silent: opts.v ? false : true
     });
     if (bootstrap.code !== 0) return false;
 
     const build = shell.exec(`docker-compose -f docker/docker-compose.yml build --no-cache`, {
         cwd: __dirname,
-        silent: options.v ? false : true
+        silent: opts.v ? false : true
     });
     if (build.code !== 0) return false;
 
     const dbinit = shell.exec(
         `docker-compose -f docker/docker-compose.yml build --no-cache \
 && ${sleep} && docker-compose -f docker/docker-compose.yml stop db`,
-        { cwd: __dirname, silent: options.v ? false : true }
+        { cwd: __dirname, silent: opts.v ? false : true }
     );
     if (dbinit.code !== 0) return false;
 
