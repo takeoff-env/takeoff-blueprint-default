c0d694e6080f549688f6d946aa9f452b92290325
Tane Piper <piper.tane@gmail.com>
2017-10-25 02:05:05 +0200

    Point to lerna bin

--- takeoff.config.js
+++ takeoff.config.js
@@ -2,7 +2,7 @@ module.exports = environment => [
     { cmd: `git submodule init`, message: `Initialising submodules`, cwd: `envs/${environment}` },
     { cmd: `git submodule update`, message: `Cloning submodules`, cwd: `envs/${environment}` },
     { cmd: `npm install`, message: `NPM Install`, cwd: `envs/${environment}` },
-    { cmd: `lerna bootstrap`, message: 'Bootstrapping environments', cwd: `envs/${environment}` },
+    { cmd: `node_modules/.bin/lerna bootstrap`, message: 'Bootstrapping environments', cwd: `envs/${environment}` },
     {
         cmd: `docker-compose -f docker/docker-compose.yml build --no-cache`,
         message: 'Running Docker Compose Build',
