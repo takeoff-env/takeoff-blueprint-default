4c0d41de26ee831bc55ea95a4493f36671d6629a
Tane Piper <piper.tane@gmail.com>
2017-10-26 00:50:24 +0200

    update dir paths

--- takeoff.config.js
+++ takeoff.config.js
@@ -6,14 +6,14 @@ module.exports = ({ command, shell, args, workingDir }) => {
     const submoduleInit = shell.exec(`npm install`, { cwd: __dirname });
     if (submoduleInit.code !== 0) return false;
 
-    const bootstrap = shell.exec(`node_modules/.bin/lerna bootstrap`);
+    const bootstrap = shell.exec(`node_modules/.bin/lerna bootstrap`, { cwd: __dirname });
     if (bootstrap.code !== 0) return false;
 
-    const build = shell.exec(`docker-compose -f docker/docker-compose.yml build --no-cache`);
+    const build = shell.exec(`docker-compose -f docker/docker-compose.yml build --no-cache`, { cwd: __dirname });
     if (build.code !== 0) return false;
 
     const dbinit = shell.exec(`docker-compose -f docker/docker-compose.yml build --no-cache \
-&& ${sleep} && docker-compose -f docker/docker-compose.yml stop db`);
+&& ${sleep} && docker-compose -f docker/docker-compose.yml stop db`, { cwd: __dirname });
     if (dbinit.code !== 0) return false;
 
     return true;
