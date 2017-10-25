1034500409cf3edef1d8a9b46f2a7009ff8fb49a
Tane Piper <piper.tane@gmail.com>
2017-10-25 10:21:55 +0200

    npm install deps

--- takeoff.config.js
+++ takeoff.config.js
@@ -5,6 +5,7 @@ module.exports = environment => [
   { cmd: `mkdir -p envs/${environment}`, message: 'Creating environment' },
   { cmd: `git submodule init`, message: `Initialising submodules`, cwd: `envs/${environment}` },
   { cmd: `git submodule update`, message: `Cloning submodules`, cwd: `envs/${environment}` },
+  { cmd: `npm install`, message: 'Installing blueprint dependencies', cwd: `envs/${environment}` },
   { cmd: `npm run bootstrap`, message: 'Bootstrapping environments', cwd: `envs/${environment}` },
   {
     cmd: `docker-compose -f docker/docker-compose.yml build --no-cache`,
