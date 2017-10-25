07d82da9959a99f9df8c2932fe45fa5e0fa17707
Tane Piper <piper.tane@gmail.com>
2017-10-25 10:19:56 +0200

    Add config file to load

---
+++ takeoff.config.js
@@ -0,0 +1,22 @@
+module.exports = environment => [
+  { cmd: `mkdir -p envs/${environment}`, message: 'Creating environment' },
+  { cmd: `git submodule init`, message: `Initialising submodules`, cwd: `envs/${environment}` },
+  { cmd: `git submodule update`, message: `Cloning submodules`, cwd: `envs/${environment}` },
+  { cmd: `npm run bootstrap`, message: 'Bootstrapping environments', cwd: `envs/${environment}` },
+  {
+    cmd: `docker-compose -f docker/docker-compose.yml build --no-cache`,
+    message: 'Running Docker Compose Build',
+    cwd: `envs/${environment}`,
+  },
+  {
+    cmd: `docker-compose -f docker/docker-compose.yml up -d db`,
+    message: 'Triggering database creation',
+    cwd: `envs/${environment}`,
+  },
+  { cmd: `${sleep}`, message: 'Waiting for database' },
+  {
+    cmd: `docker-compose -f docker/docker-compose.yml stop db`,
+    message: 'Shutting down database',
+    cwd: `envs/${environment}`,
+  },
+];
