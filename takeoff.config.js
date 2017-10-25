823023446f6810c1a15344c3f7477dc88a4810b3
Tane Piper <piper.tane@gmail.com>
2017-10-26 00:31:06 +0200

    Moved to plugin style architecture

--- takeoff.config.js
+++ takeoff.config.js
@@ -1,26 +1,40 @@
-
 let sleep = 'sleep 5';
 if (process.platform === 'win32') sleep = 'sleep -s 5';
 
-module.exports = environment => [
-  { cmd: `git submodule init`, message: `Initialising submodules`, cwd: `envs/${environment}` },
-  { cmd: `git submodule update`, message: `Cloning submodules`, cwd: `envs/${environment}` },
-  { cmd: `npm install`, message: 'Installing blueprint dependencies', cwd: `envs/${environment}` },
-  { cmd: `node_modules/.bin/lerna bootstrap`, message: 'Bootstrapping environments', cwd: `envs/${environment}` },
-  {
-    cmd: `docker-compose -f docker/docker-compose.yml build --no-cache`,
-    message: 'Running Docker Compose Build',
-    cwd: `envs/${environment}`,
-  },
-  {
-    cmd: `docker-compose -f docker/docker-compose.yml up -d db`,
-    message: 'Triggering database creation',
-    cwd: `envs/${environment}`,
-  },
-  { cmd: `${sleep}`, message: 'Waiting for database' },
-  {
-    cmd: `docker-compose -f docker/docker-compose.yml stop db`,
-    message: 'Shutting down database',
-    cwd: `envs/${environment}`,
-  },
-];
+module.exports = ({ command, shell, args, workingDir }) => {
+  const submoduleInit = shell.exec(`npm install`);
+  if (submoduleInit.code !== 0) return false;
+
+  const bootstrap = shell.exec(`node_modules/.bin/lerna bootstrap`);
+  if (bootstrap.code !== 0) return false;
+
+  const build = shell.exec(`docker-compose -f docker/docker-compose.yml build --no-cache`);
+  if (build.code !== 0) return false;
+
+  const dbinit = shell.exec(`docker-compose -f docker/docker-compose.yml build --no-cache \
+&& ${sleep} && docker-compose -f docker/docker-compose.yml stop db`);
+  if (dbinit.code !== 0) return false;
+
+  return true;
+}
+
+// module.exports = environment => [
+//   { cmd: `npm install`, message: 'Installing blueprint dependencies', cwd: `envs/${environment}` },
+//   { cmd: `node_modules/.bin/lerna bootstrap`, message: 'Bootstrapping environments', cwd: `envs/${environment}` },
+//   {
+//     cmd: `docker-compose -f docker/docker-compose.yml build --no-cache`,
+//     message: 'Running Docker Compose Build',
+//     cwd: `envs/${environment}`,
+//   },
+//   {
+//     cmd: `docker-compose -f docker/docker-compose.yml up -d db`,
+//     message: 'Triggering database creation',
+//     cwd: `envs/${environment}`,
+//   },
+//   { cmd: `${sleep}`, message: 'Waiting for database' },
+//   {
+//     cmd: `docker-compose -f docker/docker-compose.yml stop db`,
+//     message: 'Shutting down database',
+//     cwd: `envs/${environment}`,
+//   },
+// ];
