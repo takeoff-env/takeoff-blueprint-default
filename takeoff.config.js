4300bd5c243c01423659f8794a2952dfedf050a4
Tane Piper <tanepiper@users.noreply.github.com>
2017-11-22 10:51:57 +0100

    Takeoff.config.js (#12)
    * Add config file to load
    
    * Add platform sleep
    
    * npm install deps

--- takeoff.config.js
+++ takeoff.config.js
@@ -2,42 +2,45 @@ let sleep = 'sleep 5';
 if (process.platform === 'win32') sleep = 'sleep -s 5';
 
 module.exports = ({ command, shell, args, opts, workingDir, h }) => {
-    const bar = new h.progressbar({
-        schema: ' :title (:current/:total :elapseds) [:bar]',
-        total: 4
-    });
+  const bar = new h.progressbar({
+    schema: ' :title (:current/:total :elapseds) [:bar]',
+    total: 4,
+  });
 
-    bar.tick(0, { title: 'Doing NPM Install' });
+  bar.tick(0, { title: 'Doing NPM Install' });
 
-    const submoduleInit = shell.exec(`npm install`, { cwd: __dirname, silent: opts.v ? false : true });
-    if (submoduleInit.code !== 0) return false;
+  const submoduleInit = shell.exec(`npm install`, {
+    cwd: __dirname,
+    silent: opts.v ? false : true,
+  });
+  if (submoduleInit.code !== 0) return false;
 
-    bar.tick(1, { title: 'Bootstrap Environment' });
+  bar.tick(1, { title: 'Bootstrap Environment' });
 
-    const bootstrap = shell.exec(`node_modules/.bin/lerna bootstrap`, {
-        cwd: __dirname,
-        silent: opts.v ? false : true
-    });
-    if (bootstrap.code !== 0) return false;
+  const bootstrap = shell.exec(`node_modules/.bin/lerna bootstrap`, {
+    cwd: __dirname,
+    silent: opts.v ? false : true,
+  });
+  if (bootstrap.code !== 0) return false;
 
-    bar.tick(1, { title: 'Running Docker Build' });
+  bar.tick(1, { title: 'Running Docker Build' });
 
-    const build = shell.exec(`docker-compose -f docker/docker-compose.yml build --no-cache`, {
-        cwd: __dirname,
-        silent: opts.v ? false : true
-    });
-    if (build.code !== 0) return false;
+  const build = shell.exec(`docker-compose -f docker/docker-compose.yml build --no-cache`, {
+    cwd: __dirname,
+    silent: opts.v ? false : true,
+  });
+  if (build.code !== 0) return false;
 
-    bar.tick(1, { title: 'Initilising Database' });
+  bar.tick(1, { title: 'Initilising Database' });
 
-    const dbinit = shell.exec(
-        `docker-compose -f docker/docker-compose.yml build --no-cache \
+  const dbinit = shell.exec(
+    `docker-compose -f docker/docker-compose.yml build --no-cache \
 && docker-compose -f docker/docker-compose.yml up -d db && ${sleep} && docker-compose -f docker/docker-compose.yml stop db`,
-        { cwd: __dirname, silent: opts.v ? false : true }
-    );
-    if (dbinit.code !== 0) return false;
+    { cwd: __dirname, silent: opts.v ? false : true },
+  );
+  if (dbinit.code !== 0) return false;
 
-    bar.tick(1, { title: 'Done' });
+  bar.tick(1, { title: 'Done' });
 
-    return true;
+  return true;
 };
